5c874c3043048c28dd0fb48f3516bb1dcff23a9d
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2025-05-13 18:02:36 +0200

    move files

---
+++ roundabout4.jl
@@ -0,0 +1,35 @@
+using CSV, DataFrames
+using SumOfSquares, MosekTools
+using MultivariateMoments
+using DynamicPolynomials
+using LinearAlgebra
+using PGFPlots
+
+D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
+    (d -> d[:,["x","y","vx","vy"]]) |>
+    (d -> d .- [1000 1000 0 0]) |>
+    (d -> d ./ [20 20 20 20]) |>
+    (d -> filter(e -> -1 <= e[1] <= 1, d)) |>
+    (d -> filter(e -> -1 <= e[2] <= 1, d))
+
+@polyvar x[1:4] u[1:2]
+f = [x[3:4]; u]
+d = 4
+φ = monomials(x,0:2d)
+
+M = sum(dirac(φ,(x.=>collect(s))...) for s in eachrow(D)) * (1/size(D,1))
+Q = let v = monomials(x,0:d)
+    v'*inv(expectation.(v*v',[M]))*v
+end
+
+m = SOSModel(Mosek.Optimizer)
+@variable m V Poly(φ)
+@objective m Max V(0.2,-1.0,0.0, 0.0) - V(-1.0, 0.8, 0.0, 0.0)
+c = @constraint m differentiate(V,x)'*f >= -Q domain=@set([x;u]'*[x;u]<=10)
+optimize!(m)
+
+μ = dual(c)
+q = let v = monomials(x[1:2],0:d)
+    v'*inv(expectation.(v*v',[μ]))*v
+end
+save("roundabout4.pdf", Plots.Image((x...)->-log(q(x...)),(-1,1),(-1,1)))
--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ julia --project diffusion.jl
 julia --project diffusion2.jl
 julia --project roundabout.jl
 julia --project roundabout2.jl
+julia --project roundabout3.jl
+julia --project roundabout4.jl
+
+mv *.pdf fig/
